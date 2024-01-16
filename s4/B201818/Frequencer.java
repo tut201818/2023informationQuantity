8e0b4feca57d2c8b5dc1c8c5e29580fd2aca2bca
tut201818 <153586530+tut201818@users.noreply.github.com>
2024-01-16 18:15:32 +0900

    Update Frequencer.java

--- s4/B201818/Frequencer.java
+++ s4/B201818/Frequencer.java
@@ -259,7 +259,7 @@ public class Frequencer implements FrequencerInterface {
                 myObject.setTarget("H".getBytes());//探す単語をセット
                 freq = myObject.frequency();
 
-		/*myObject = new Frequencer();
+		myObject = new Frequencer();
                 myObject.setSpace("ABC".getBytes());
                 myObject.printSuffixArray();
                 myObject = new Frequencer();
@@ -271,7 +271,7 @@ public class Frequencer implements FrequencerInterface {
                 myObject = new Frequencer();
                 myObject.setSpace("Hi Ho Hi Ho".getBytes());
                 myObject.printSuffixArray();
-		*/
+		
         }
         catch(Exception e) {
             System.out.println("Exception occurred: STOP");
