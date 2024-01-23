bd1fc60bbbd6183f798b16810f1d31240ad8bec6
tut201818 <153586530+tut201818@users.noreply.github.com>
2024-01-23 15:11:41 +0900

    Update TestCase.java

--- s4/B201818/TestCase.java
+++ s4/B201818/TestCase.java
@@ -49,25 +49,25 @@ public class TestCase {
 	    myObject.setSpace("Hi Ho H".getBytes());
 	    myObject.setTarget("H".getBytes());
 	    freq = myObject.frequency();
-	    assert freq == 3 : "Hi Ho Hi Ho, H: " + freq;
+	    assert freq == 3 : "Hi Ho H, H: " + freq;
 		
 	    myObject = new Frequencer();
 	    myObject.setSpace("Hi Ho Hi Ho".getBytes());
 	    myObject.setTarget("Hi Ho".getBytes());
 	    freq = myObject.frequency();
-	    assert freq == 2 : "Hi Ho Hi Ho, H: " + freq;
+	    assert freq == 2 : "Hi Ho Hi Ho, Hi Ho: " + freq;
 		
 	    myObject = new Frequencer();
 	    myObject.setSpace("Hi Ho Hi Ho".getBytes());
 	    myObject.setTarget("Hi Ho H".getBytes());
 	    freq = myObject.frequency();
-	    assert freq == 1 : "Hi Ho Hi Ho, H: " + freq;
+	    assert freq == 1 : "Hi Ho Hi Ho, Hi Ho H: " + freq;
 		
 	    myObject = new Frequencer();
 	    myObject.setSpace("Hi Ho Hi Ho".getBytes());
-	    myObject.setTarget("Hi Ho Hi Ho".getBytes());
+	    myObject.setTarget("Hi Ho Hi Ho Hi Ho".getBytes());
 	    freq = myObject.frequency();
-	    assert freq == 1 : "Hi Ho Hi Ho, H: " + freq;
+	    assert freq == 0 : "Hi Ho Hi Ho, Hi Ho Hi Ho Hi Ho: " + freq;
 		
 	    // 空のターゲットでテスト
 	    myObject.setTarget("".getBytes());
