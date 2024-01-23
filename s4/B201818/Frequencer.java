11511e6799c486e6ecffc99ac94f93309b0b969a
tut201818 <153586530+tut201818@users.noreply.github.com>
2024-01-23 16:44:11 +0900

    Update Frequencer.java

--- s4/B201818/Frequencer.java
+++ s4/B201818/Frequencer.java
@@ -210,9 +210,8 @@ public class Frequencer implements FrequencerInterface {
 	int i,j;
 	for (i = 0;i<mySpace.length;i++){
 	    boolean abort = false;
-	　　if((suffixArray[i] + (end-start)) < mySpace.length){//文字数を超過していれば
+            if((suffixArray[i] + (end-start)) < mySpace.length){//文字数を超過していれば
             for(j = 0; j<(end-start); j++) {//開始地点からターゲットと一致しているか一文字ずつ調べる。
-		
                 if(myTarget[start+j] != mySpace[suffixArray[i] + j]) { abort = true; break; }//一文字でも一致していなければbreak
             	}
             if(abort == false) { 
@@ -254,7 +253,7 @@ public class Frequencer implements FrequencerInterface {
 	int i,j;
 	for (i = 0;i<mySpace.length;i++){
 	    boolean abort = false;
-	    if((suffixArray[i] + (end-start)) < mySpace.length){//文字数を超過していれば
+            if((suffixArray[i] + (end-start)) < mySpace.length){//文字数を超過していれば
             for(j = 0; j<(end-start); j++) {//開始地点からターゲットと一致しているか一文字ずつ調べる。
                 if(myTarget[start+j] != mySpace[suffixArray[i]+j]) { abort = true; break; }//一文字でも一致していなければbreak
             }
@@ -267,14 +266,14 @@ public class Frequencer implements FrequencerInterface {
 	//カウントが進んだ状態からスタート
 	for (i = i;i<mySpace.length;i++){
 	    boolean abort = true;
-	    if((suffixArray[i] + (end-start)) < mySpace.length){return i;}//文字数を超過していれば
+            if((suffixArray[i] + (end-start)) < mySpace.length){return i;}//文字数を超過していれば
             for(j = 0; j<(end-start); j++) {//開始地点からターゲットと一致しているか一文字ずつ調べる。
                 if(myTarget[start+j] != mySpace[suffixArray[i]+j]) { abort = false; break; }//一文字でも一致していなければbreak
             }
             if(abort == false) { 
 		return i;
 	    }//不一致だった時にリターン
-        }                           
+            }
         return -1;
     }
 
